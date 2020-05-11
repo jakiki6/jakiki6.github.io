b4cd1a2bfcf7633385c204e6aab318bd331bd1df
Jakob Kirsch <jakob.kirsch@teckids.org>
2020-05-11 13:25:04 +0200

    add graph

---
+++ graph/graph.js
@@ -0,0 +1,38 @@
+function drawLine(ctx, startX, startY, endX, endY){
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(endX,endY);
+    ctx.stroke();
+}
+
+function draw() {
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle = "#FFFFFF";
+	ctx.strokeStyle = "#0000FF";
+	var form = document.getElementById("form").value;
+	var maxX = document.getElementById("maxX").value;
+	var minX = document.getElementById("minX").value;
+	var step = document.getElementById("step").value;
+	var minY = 0;
+	var maxY = 0;
+	var y = 0;
+	for (var x = minX; x <= maxX; x = x + step) {
+		eval(form);
+		if (y > maxY) {
+			maxY = y;
+		}
+		if (y < minY) {
+			minY = y
+		}
+	}
+	ctx.width = maxX + Math.abs(minX);
+	ctx.height = maxY + Math.abs(minY);
+	var px = minX;
+	var py = minY;
+	for (var x = minX; x <= maxX; x = x + step) {
+		eval(form);
+		drawLine(ctx, px, py, x, y);
+	}
+	alert("Done!");
+}
