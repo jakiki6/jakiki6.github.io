222a98d4bcf0d2f6af59864443cb6e71b51a4622
Jakob Kirsch <jakob.kirsch@web.de>
2020-06-09 11:04:36 +0200

    fix

--- debian/conf.sh
+++ debian/conf.sh
@@ -50,10 +50,10 @@ chmod +x /sbin/verity
 
 cat << EOF >> /sbin/verity
 
-if [ "\$(cat \$(df /boot | tail -n +2 | cut -d' ' -f1) | b3sum | sed -e 's/ -//g')" != "\$hash" ]; then
+if [ "\$(cat \$(df /boot | tail -n +2 | cut -d' ' -f1) | b3sum | sed -e 's/ -//g')" = \$hash ]; then
 	echo Everything ok
 else
-	echo 64 > /proc/sysrq-trigger
+	echo e > /proc/sysrq-trigger
 fi
 EOF
 
